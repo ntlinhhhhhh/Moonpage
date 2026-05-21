08050dd9291a2558ceab57e96d3ee886a261947b
l4reve <zmint2254@gmail.com>
2026-05-21 22:53:07 +0700

    fix

--- frontend/moonpage/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_diary_moonpage_MoonPageApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.diary.moonpage.MoonPageApplication"
-)
-public final class _com_diary_moonpage_MoonPageApplication {
-}
